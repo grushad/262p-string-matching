37737d2ae8f08504f53b1847348ea339664a73d1
grushad <grushadharod@gmail.com>
2023-04-28 14:17:16 -0700

    initial commit

---
+++ project2/Brute.java
@@ -0,0 +1,10 @@
+package project2;
+
+public class Brute {
+
+  public int match(String T, String P) {
+    /** Your code goes here */
+    return -1;
+  }
+  
+}
---
+++ project2/KMP.java
@@ -0,0 +1,10 @@
+package project2;
+
+public class KMP {
+  
+  public int match(String T, String P) {
+    /** Your code goes here */
+    return -1;
+  }
+
+}
