9ea1a66a7ea3adc843c11f52d49a80dddb8bddb9
grushad <grushadharod@gmail.com>
2023-04-28 14:34:14 -0700

    brute algo impl

--- project2/Brute.java
+++ project2/Brute.java
@@ -4,6 +4,18 @@ public class Brute {
 
   public int match(String T, String P) {
     /** Your code goes here */
+    int n = T.length();
+    int m = P.length();
+    if(m > n)
+      return -1;
+    for(int i = 0; i <= (n - m); i++){
+      int j = 0;
+      while(j < m && T.charAt(i + j) == P.charAt(j)){
+        j++;
+      }
+      if(j == m)
+        return i;      
+    }
     return -1;
   }
   
